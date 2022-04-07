142c68da18646464a114f59df87930ad8a7c943b
Vladislav <kovalvlad9@gmail.com>
2022-04-07 19:17:31 +0300

    add validation

--- src/Pages/ Login.jsx
+++ src/Pages/ Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
 async function loginUser(credentials) {
@@ -12,9 +12,77 @@ async function loginUser(credentials) {
         .then(data => data.json())
 }
 
+
+
+
+const useValidation = (value, validations) => {
+    const [isEmpty, setIsEmpty] = useState(true)
+    const [inputValid, setInputValid] = useState(false)
+    const [emailError, setEmailError] = useState(false)
+    const [passError, setPassError] = useState(false)
+    const [minLengthError, setMinLengthError] = useState(false)
+
+    useEffect(()=>{
+        for (const validation in validations){
+            switch (validations){
+                case 'minLength':
+                    value.length < validations[validations] ?setMinLengthError(true):setMinLengthError(false)
+                    break
+                case 'isEmpty':
+                    value ? setIsEmpty(false) : setIsEmpty(true)
+                    break
+                case 'passError':
+                    const regexp = /^[a-z\s]+$/i;
+                    regexp.test(String(value).toLowerCase()) ? setPassError(true) : setPassError(false)
+                    break
+                case 'emailError':
+                    const reg = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu
+                    reg.test(String(value).toLowerCase()) ? setEmailError(false) : setEmailError(true)
+                    break
+            }
+        }
+    },[value])
+
+    useEffect(()=>{
+        if(isEmpty || minLengthError || emailError || passError){
+            setInputValid(false)
+        }else {
+            setInputValid(true)
+        }
+
+    },[isEmpty,minLengthError,emailError,passError])
+    return{
+        isEmpty,
+        minLengthError,
+        emailError,
+        passError,
+        inputValid
+    }
+
+}
+
+const useInput = (initialValue, validations) => {
+    const [value, setValue] = useState(initialValue)
+    const [isDirty, setIsDirty] = useState(false)
+    const valid = useValidation(value,validations)
+    const onChange = (e) => {
+        setValue(e.target.value)
+    }
+    const onBlur = (e) => {
+        setIsDirty(true)
+    }
+    return{
+        value,
+        onChange,
+        onBlur,
+        isDirty,
+        ...valid
+    }
+}
+
 export default function Login({ setToken }) {
-    const [username, setUserName] = useState();
-    const [password, setPassword] = useState();
+    const [username, setUserName] = useState()
+    const [password, setPassword] = useState()
 
     const handleSubmit = async e => {
         e.preventDefault();
@@ -25,20 +93,38 @@ export default function Login({ setToken }) {
         setToken(token);
     }
 
+
+
+
+    const email = useInput('', {isEmpty:true,emailError:true })
+    const pass = useInput('',{isEmpty:true,passError:true,minLengthError:8 })
+
+    const handleChangePass = (e) => {
+        setPassword(e.target.value)
+        pass.onChange(e)
+
+    }
+    const handleChangeEmail = (e) => {
+        setUserName(e.target.value)
+        email.onChange(e)
+
+    }
+
+
     return(
         <div className="login-wrapper">
             <h1>Please Log In</h1>
             <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input type="text" onChange={e => setUserName(e.target.value)} />
+                    <input type="text" onChange={e => handleChangeEmail(e)} />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" onChange={e => setPassword(e.target.value)} />
+                    <input type="password" onChange={e => handleChangePass(e)} />
                 </label>
                 <div>
-                    <button type="submit">Submit</button>
+                    <button disabled={!email.inputValid||!pass.inputValid} type="submit">Submit</button>
                 </div>
             </form>
         </div>
