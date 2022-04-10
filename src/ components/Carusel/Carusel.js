6f30213a9b6ec10b27a10eac2f474b044e05c932
Vladislav <kovalvlad9@gmail.com>
2022-04-10 20:35:38 +0300

    slider

--- src/ components/Carusel/Carusel.js
+++ src/ components/Carusel/Carusel.js
@@ -9,29 +9,28 @@ import './Carusel.css'
 
 // import "./styles.css";
 import { Pagination } from "swiper";
-import * as url from "url";
 
 export default function Carusel() {
     return (
         <>
             <Swiper
-                slidesPerView={3}
-                spaceBetween={30}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Pagination]}
+                slidesPerView={3.3}
+                spaceBetween={5}
+                // pagination={{
+                //     clickable: true,
+                // }}
+                // modules={[Pagination]}
                 className="mySwiper"
             >
-                <SwiperSlide style={{backgroundImage:url('')}}>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+                <SwiperSlide className='slide1'></SwiperSlide>
+                <SwiperSlide className='slide2'></SwiperSlide>
+                <SwiperSlide className='slide3'></SwiperSlide>
+                <SwiperSlide className='slide4'></SwiperSlide>
+                <SwiperSlide className='slide1'></SwiperSlide>
+                <SwiperSlide className='slide2'></SwiperSlide>
+                <SwiperSlide className='slide3'></SwiperSlide>
+                <SwiperSlide className='slide4'></SwiperSlide>
+
             </Swiper>
         </>
     );
